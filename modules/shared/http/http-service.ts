8f5793d538bc0e1c1d681d30cd9eb3b3ffe62c14
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2026-04-14 01:27:27 +0700

    refactor: remove ApiError class from http-service module

--- modules/shared/http/http-service.ts
+++ modules/shared/http/http-service.ts
@@ -86,15 +86,3 @@ export abstract class HttpService {
     } as unknown as IResponse<T>;
   }
 }
-
-export class ApiError extends Error {
-  status: number;
-  payload?: unknown;
-
-  constructor(message: string, status: number, payload?: unknown) {
-    super(message);
-    this.name = "ApiError";
-    this.status = status;
-    this.payload = payload;
-  }
-}
