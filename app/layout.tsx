19803c9eb933c921fb2a6d7be31161f544283537
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2026-04-13 23:57:24 +0700

    feat: add Toaster component to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "@/styles/globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const raleway = Raleway({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-raleway',
-  weight: ['400', '500', '600', '700'],
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-raleway",
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Customer Mini App",
@@ -20,11 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${raleway.variable} h-full antialiased`}
-    >
-      <body className={`${raleway.className} min-h-full flex flex-col`}>{children}</body>
+    <html lang="en" className={`${raleway.variable} h-full antialiased`}>
+      <body className={`${raleway.className} min-h-full flex flex-col`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
