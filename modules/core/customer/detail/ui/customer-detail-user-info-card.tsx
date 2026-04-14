3ef0dec9f61fd2bcdb026a6fea95b573c7fafa5e
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2026-04-14 12:33:19 +0700

    feat: add status indicator icon to customer user info card

--- modules/core/customer/detail/ui/customer-detail-user-info-card.tsx
+++ modules/core/customer/detail/ui/customer-detail-user-info-card.tsx
@@ -8,10 +8,11 @@ export const CustomerDetailUserInfoCard = () => {
   return (
     <div className="bg-white rounded-4xl shadow-sm p-8">
       <div className="flex flex-col space-y-4 justify-center items-center">
-        <div className="bg-[#AFEFDD] w-[128px] h-[128px] rounded-full flex items-center justify-center">
+        <div className="bg-[#AFEFDD] w-[128px] h-[128px] rounded-full flex items-center justify-center relative">
           <span className="text-4xl font-bold text-black">
             {data?.initials}
           </span>
+          <div className="absolute bottom-0 right-0 border-3 border-white bg-primary rounded-full p-2 w-8 h-8 flex items-center justify-center"></div>
         </div>
         <h4 className="text-2xl font-bold text-black mb-2">
           {data?.name || "-"}
