052750c507019bf2a2e392d8d19e0a24eedb4e84
cn-pitchayakorn <yo.pitchayakorn@gmail.com>
2026-04-22 00:22:11 +0700

    fix: redirect by user session

--- modules/core/auth/login/ui/login-view.tsx
+++ modules/core/auth/login/ui/login-view.tsx
@@ -1,8 +1,20 @@
 "use client";
 
+import { redirect } from "next/navigation";
+import { useAuthStore } from "../../store/auth-store";
 import { LoginCard } from "./login-card";
+import { useEffect } from "react";
 
 export const LoginView = () => {
+
+  const { isAuthenticated } = useAuthStore()
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      redirect("/customer")
+    }
+  }, [isAuthenticated])
+
   return (
     <div className="bg-bg-main min-h-screen flex flex-col justify-center items-center">
       <LoginCard />
